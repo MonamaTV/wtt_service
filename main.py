1ce18080fb7eed2c14cbc3a5875003cb035648cd
Tadima <19066791722k23@bootcamp.wethinkcode.co.za>
2024-04-21 20:25:26 +0200

    trying to reduce loading time

--- main.py
+++ main.py
@@ -61,7 +61,3 @@ async def root():
     return {"message": "Server is running!"}
 
 
-if __name__ == "__main__":
-    import uvicorn
-
-    uvicorn.run("main:app", host="127.0.0.1", port=3000, log_level="info")
